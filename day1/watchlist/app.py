486be6426a9380239b97d7fe22d7e18f9da94afe
shuai <1312475697@qq.com>
2020-05-07 13:42:26 +0800

    静态资源

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -12,6 +12,18 @@ def index():
         {"title":"西华路放","year":"2020"},
         {"title":"速度与激情8","year":"2020"},
         {"title":"我和我的祖国","year":"2020"},
+        {"title":"大赢家","year":"2020"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼 ","year":"2020"},
+        {"title":"西华路放","year":"2020"},
+        {"title":"速度与激情8","year":"2020"},
+        {"title":"我和我的祖国","year":"2020"},
+        {"title":"大赢家","year":"2020"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼 ","year":"2020"},
+        {"title":"西华路放","year":"2020"},
+        {"title":"速度与激情8","year":"2020"},
+        {"title":"我和我的祖国","year":"2020"},
     ]
     return render_template('index.html',name=name,movies=movies)
 
