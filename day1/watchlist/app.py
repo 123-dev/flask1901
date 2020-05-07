ee4a0a8933074b7d0e44ba310e4db5456beff506
shuai <1312475697@qq.com>
2020-05-07 09:32:13 +0800

    增加首页模板

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return "<h1>Hello,flask 中国</h1>"
+    name = "Bruce"
+    movies = [
+        {"title":"大赢家","year":"2020"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼 ","year":"2020"},
+        {"title":"西华路放","year":"2020"},
+        {"title":"速度与激情8","year":"2020"},
+        {"title":"我和我的祖国","year":"2020"},
+    ]
+    return render_template('index.html',name=name,movies=movies)
 
-#动态路由
-@app.route('/index/<name>')
-def home(name):
-    return "<h1>Hello,flask, %s</h1>"%name
+# #动态路由
+# @app.route('/index/<name>')
+# def home(name):
+#     return "<h1>Hello,flask, %s</h1>"%name
