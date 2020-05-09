98610f08ebd847997278835e92742383f72f9c84
shuai <1312475697@qq.com>
2020-05-09 12:02:19 +0800

    数据初始化完成，数据导入 成功

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -17,7 +17,7 @@ else:
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')    
-app.config['SQLALCHY_TRACK_MODIFICATIONS'] = False  #关闭了对模型修改的监控 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  #关闭了对模型修改的监控 
 
 db = SQLAlchemy(app)    #初始化扩展，传入程序实例app
 
@@ -33,6 +33,26 @@ class Movie(db.Model):
 @app.route('/')
 
 def index():
+    
+    user = User.query.first()
+    movies = Movie.query.all()
+
+    return render_template('index.html',user=user,movies=movies)
+
+
+#自定义命令
+# 新建data.db的数据库初始化命令
+@app.cli.command()   #装饰器，注册命令
+@click.option('--drop',is_flag=True,help='删除之后再创建')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("初始化数据库完成")
+
+# 向data.db中写入数据的命令
+@app.cli.command()
+def forge():
     name = "Bruce"
     movies = [
         {"title":"大赢家","year":"2020"},
@@ -54,15 +74,11 @@ def index():
         {"title":"速度与激情8","year":"2020"},
         {"title":"我和我的祖国","year":"2020"},
     ]
-    return render_template('index.html',name=name,movies=movies)
-
-
-#自定义命令
-@app.cli.command()   #装饰器，注册命令
-@click.option('--drop',is_flag=True,help='删除之后再创建')
-def initdb(drop):
-    if drop:
-        db.drop_all()
-    db.create_all()
-    click.echo("初始化数据库完成")
-
+    
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo("插入数据完成")
